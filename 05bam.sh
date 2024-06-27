556b98e9f3377f4e07559e9519804603e3fb8a6a
WanghcPKU <165775424+WanghcPKU@users.noreply.github.com>
2024-06-27 21:43:10 +0800

    Create 05bam.sh

---
+++ 05bam.sh
@@ -0,0 +1,9 @@
+ls ./hisat/ | while read id
+do
+name=${id##*/}
+neme=${name%%.sam}".bam"
+nume=${name%%.sam}"_sorted.bam"
+samtools view -@ 20 -S ./hisat/$id -b > ./bam2/$neme #-@ thread
+samtools sort -@ 20 ./bam2/$neme -o ./bam2/$nume
+samtools index $nume
+done
