2f2ba0f7e2ea7d398c950d9474b24fca79928318
WanghcPKU <165775424+WanghcPKU@users.noreply.github.com>
2024-06-28 19:11:49 +0800

    Update 05bam.sh

--- 05bam.sh
+++ 05bam.sh
@@ -5,5 +5,5 @@ neme=${name%%.sam}".bam"
 nume=${name%%.sam}"_sorted.bam"
 samtools view -@ 20 -S ./hisat/$id -b > ./bam2/$neme #-@ thread
 samtools sort -@ 20 ./bam2/$neme -o ./bam2/$nume
-samtools index $nume
+samtools index ./bam2/$nume
 done
